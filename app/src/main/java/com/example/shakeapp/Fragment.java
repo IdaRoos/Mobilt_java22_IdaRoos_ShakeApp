5939ac4a8282f7266aff89856267275b28fc9cc8
IdaRoos <ida_r@live.se>
2023-08-31 19:27:47 +0200

    Code cleaned up

--- app/src/main/java/com/example/shakeapp/Fragment.java
+++ app/src/main/java/com/example/shakeapp/Fragment.java
@@ -10,16 +10,12 @@ import android.widget.TextView;
 
 public class Fragment extends androidx.fragment.app.Fragment {
 
-   // private TextView sensorValueTextView;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_sensor_data, container, false);
 
-        // Hitta referens till TextView i layouten
-      //  sensorValueTextView = rootView.findViewById(R.id.textviewfragment);
 
         return rootView;
     }
